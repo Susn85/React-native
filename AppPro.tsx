fbdc043e9287d01c21f76eb95612d0a8d33ce74d
sujeet nayak <147132954+Susn85@users.noreply.github.com>
2026-02-22 20:32:57 +0530

    Update Google Sign-In config and clean up code
    Updated Google Sign-In configuration with a placeholder web client ID and removed commented-out code for clarity.

--- AppPro.tsx
+++ AppPro.tsx
@@ -10,10 +10,9 @@ import { PersistGate} from 'redux-persist/integration/react';
 
 GoogleSignin.configure({
   webClientId:
-'1024477844174-dfed5plu8jbns3b4v854p7gpafu3qtdt.apps.googleusercontent.com',
+   'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
   forceCodeForRefreshToken:true,
   offlineAccess:true,
-  // iosClientId:"1024477844174-0k6a1r6v6b5j5q1bq7v7j9b5k9j2b7v7.apps.googleusercontent.com",
 });
 const AppPro = () => {
   return (
@@ -31,34 +30,3 @@ const AppPro = () => {
 };
 
 export default AppPro;
-
-
-
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import SocialLogin from './SocialLogin';
-// import { AuthProvider } from './screen/AuthContext';
-// import HomeScreen from './screen/HomeScreen'; 
-// import { GoogleSignin } from '@react-native-google-signin/google-signin';
-
-// GoogleSignin.configure({
-//   webClientId:"1024477844174-dfed5plu8jbns3b4v854p7gpafu3qtdt.apps.googleusercontent.com",
-//   offlineAccess: true,
-// });
-// const Stack = createStackNavigator();
-
-// const AppPro = () => {
-//   return (
-//     <AuthProvider>
-//       <NavigationContainer>
-//         <Stack.Navigator>
-//           <Stack.Screen name="Login" component={SocialLogin} />
-//           <Stack.Screen name="Home" component={HomeScreen} />
-//         </Stack.Navigator>
-//       </NavigationContainer>
-//     </AuthProvider>
-//   );
-// };
-
-// export default AppPro;
